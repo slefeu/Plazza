473acd400224f47e0f543439a67889181caf06db
Solène Lefeu <solene.lefeu@epitech.eu>
2022-05-05 00:44:13 +0200

    [ADD] | Kitchen constructor
    to initialize all my necessary attributes

--- Kitchen/Kitchen.cpp
+++ Kitchen/Kitchen.cpp
@@ -6,3 +6,15 @@
 */
 
 #include "Kitchen.hpp"
+
+namespace plazza
+{
+Kitchen::Kitchen(
+    unsigned int nb_cooks, int multiplier, unsigned int restock_time) noexcept
+    : cooks_(threads::ThreadPool(nb_cooks))
+    , max_pizza_(nb_cooks * 2)
+    , multiplier_(multiplier)
+    , fridge_(Fridge(restock_time))
+{
+}
+} // namespace plazza
--- Kitchen/Kitchen.hpp
+++ Kitchen/Kitchen.hpp
@@ -7,18 +7,33 @@
 
 #pragma once
 
+#include "Clock.hpp"
+#include "Fridge.hpp"
+#include "Threads.hpp"
+
+namespace plazza
+{
+
 class Kitchen
 {
-public:
-    explicit Kitchen(unsigned int cooks) noexcept;
-    Kitchen(const Kitchen &other) noexcept = default;
-    Kitchen(Kitchen &&other) noexcept = default;
+  public:
+    explicit Kitchen(unsigned int nb_cooks,
+        int multiplier,
+        unsigned int restock_time) noexcept;
+    Kitchen(const Kitchen& other) noexcept = delete;
+    Kitchen(Kitchen&& other) noexcept = default;
     ~Kitchen() noexcept = default;
 
-    Kitchen &operator=(const Kitchen &rhs) noexcept = default;
-    Kitchen &operator=(Kitchen &&rhs) noexcept = default;
+    Kitchen& operator=(const Kitchen& rhs) noexcept = delete;
+    Kitchen& operator=(Kitchen&& rhs) noexcept = default;
 
-protected:
-private:
-    unsigned int nbCooks = 0;
+  protected:
+  private:
+    threads::ThreadPool cooks_;
+    unsigned int max_pizza_;
+    int multiplier_;
+    bool full_{false};
+    Clock clock_;
+    Fridge fridge_;
 };
+} // namespace plazza
