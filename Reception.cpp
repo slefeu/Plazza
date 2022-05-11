4559232c556daa62a663d2f90ff0147b7d541cdd
Unknown <bobbax4@live.fr>
2022-05-11 03:10:43 +0200

    [ADD] | Added pizza parsing, syntax checking and started PizzaFactory

--- Reception.cpp
+++ Reception.cpp
@@ -38,17 +38,21 @@ Reception::Reception(char** av)
     multiplier_ = parseArgument(av[1]);
     cooks_ = static_cast<int>(parseArgument(av[2]));
     time_ = static_cast<int>(parseArgument(av[3]));
+    pizzaTypes_ = {"margarita", "regina", "americana", "fantasia"};
+    pizzaSizes_ = {"S", "M", "L", "XL", "XXL"};
 }
 
 void Reception::setUserInput() noexcept
 {
     std::string command;
+    std::istream& stream = std::cin;
 
-    if (getline(std::cin, command) && std::cin.good())
-        command_ = command;
-    else {
+    getline(stream, command);
+    if (stream.eof() || std::cin.fail()) {
         command_ = "";
         exit();
+    } else {
+        command_ = command;
     }
 }
 
@@ -65,6 +69,96 @@ void Reception::status()
 {
 }
 
+std::string& Reception::trim(std::string& string)
+{
+    string.erase(string.begin(),
+        std::find_if(string.begin(),
+            string.end(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))));
+    string.erase(std::find_if(string.rbegin(),
+                     string.rend(),
+                     std::not1(std::ptr_fun<int, int>(std::isspace)))
+                     .base(),
+        string.end());
+    return (string);
+}
+
+bool Reception::checkPizzaType(std::string& type)
+{
+    if (std::find_if_not(type.begin(), type.end(), ::isalpha) != type.end())
+        return (false);
+    if (std::find(pizzaTypes_.begin(), pizzaTypes_.end(), type)
+        == pizzaTypes_.end())
+        return (false);
+    return (true);
+}
+
+bool Reception::checkPizzaSize(std::string& size)
+{
+    if (std::find(pizzaSizes_.begin(), pizzaSizes_.end(), size)
+        == pizzaSizes_.end())
+        return (false);
+    return (true);
+}
+
+bool Reception::checkPizzaNumber(std::string number)
+{
+    if (number.at(0) != 'x')
+        return (false);
+    number.erase(0, 1);
+    if (number.empty() || isdigit(number.at(0)) == 0 || number.at(0) == '0')
+        return (false);
+    number.erase(0, 1);
+    if (std::find_if_not(number.begin(), number.end(), ::isdigit)
+               != number.end())
+        return (false);
+    return (true);
+}
+
+bool Reception::checkOrder(std::string &order)
+{
+    std::stringstream stream(trim(order));
+    std::string type;
+    std::string size;
+    std::string number;
+
+    try {
+        stream >> type >> size >> number;
+        if (type.empty() || size.empty() || number.empty())
+            throw(ExecutionError("Incorrect order : " + order));
+        if (!checkPizzaType(type))
+            throw(ExecutionError("Incorrect pizza type : " + type));
+        if (!checkPizzaSize(size))
+            throw(ExecutionError("Incorrect pizza size : " + size));
+        if (!checkPizzaNumber(number))
+            throw(ExecutionError("Incorrect pizza number : " + number));
+    } catch (const ExecutionError &ex) {
+        std::cerr << "Error : " << ex.what() << std::endl;
+        return (false);
+    }
+    return (true);
+}
+
+void Reception::orderPizza(std::string &order)
+{
+    
+}
+
+void Reception::checkOrderSyntax()
+{
+    size_t pos = 0;
+    std::string order;
+
+    while ((pos = command_.find(';')) != std::string::npos) {
+        order = command_.substr(0, pos);
+        if (checkOrder(order))
+            orderPizza(order);
+        command_.erase(0, pos + 1);
+    }
+    if (checkOrder(command_))
+        orderPizza(order);
+}
+
 void Reception::executeCommand()
 {
     static std::map<std::string, void (Reception::*)()> creator = {
@@ -74,32 +168,18 @@ void Reception::executeCommand()
     };
     // rajouter la commande pour lancer la commande de pizzas
 
-    command_.erase(
-        std::remove(command_.begin(), command_.end(), ' '), command_.end());
-    command_.erase(
-        std::remove(command_.begin(), command_.end(), '\t'), command_.end());
-
+    command_ = trim(command_);
     auto iterator = creator.find(command_);
 
+    if (command_.empty())
+        return;
     if (iterator != creator.end())
         return (((*this).*(iterator->second))());
-    if (!command_.empty())
-        throw(ExecutionError("Command not found."));
+    checkOrderSyntax();
 }
-
-namespace
-{
-std::function<void()> shutdownHandler;
-void signalHandler(__attribute__((unused)) int signal)
-{
-    shutdownHandler();
-}
-} // namespace anonyme pour faire passer le contexte
 
 void Reception::executeShell()
 {
-    shutdownHandler = [this]() { this->exit(); };
-    std::signal(SIGINT, signalHandler);
     while (!isEnd_) {
         std::cout << "> ";
         try {
--- Reception.hpp
+++ Reception.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace plazza
 {
@@ -23,18 +24,27 @@ class Reception
     Reception& operator=(Reception&& rhs) noexcept = default;
 
     void executeShell();
+    static std::string &trim(std::string &);
 
   protected:
   private:
     // methods
     static double parseArgument(const std::string& str);
+    static bool checkPizzaNumber(std::string);
+    bool checkPizzaType(std::string &);
+    bool checkPizzaSize(std::string &);
     void executeCommand();
+    void orderPizza(std::string &);
+    void checkOrderSyntax();
+    bool checkOrder(std::string &);
     void setUserInput() noexcept;
     void exit() noexcept;
     void log();
     void status();
     // attributes
     std::string command_;
+    std::vector<std::string> pizzaTypes_;
+    std::vector<std::string> pizzaSizes_;
     int cooks_ = 0;
     double multiplier_ = 0;
     int time_ = 0;
